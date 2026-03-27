c168225213a35556cf0efbbefdaf7a713b553823
AhmedAzzi <mrahmedazzi@gmail.com>
2026-03-27 17:23:42 +0100

    fix: switch to BrowserRouter with basename for anchor navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client"; 
 import App from "./App.tsx"; 
 import "./index.css"; 
-import { HashRouter } from "react-router-dom"; // <-- add this
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <BrowserRouter basename="/mcazzi/">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
