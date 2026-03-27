6b551f7857057d5fd80675924c80b4a098e59b07
AZZI MOHAMED CHERIF <mcazzi.prod@gmail.com>
2026-03-27 21:47:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/mcazzi/",
+  base: "/mcazzi/", // 👈 هذا هو الحل
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
